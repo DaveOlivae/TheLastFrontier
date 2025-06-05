026c43fbf853227be3a01941c7d7279ad2f1cfb9
DaveOlivae <davioliveiramelo2604@gmail.com>
2025-06-04 23:57:47 -0300

    terminado

--- src/game/environments/Plains.java
+++ src/game/environments/Plains.java
@@ -1,6 +1,7 @@
 package game.environments;
 
 import game.entity.enemies.EN_RaiderPurple;
+import game.events.eventoDoencaFerimento.Fever;
 import game.graphics.GamePanel;
 import game.graphics.TileManager;
 import game.itens.food.Apple;
@@ -20,41 +21,49 @@ public class Plains extends Environment{
                 setEnvPath("/maps/plains0.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 1:
                 setEnvPath("/maps/plains1.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 2:
                 setEnvPath("/maps/plains2.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 3:
                 setEnvPath("/maps/plains3.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 4:
                 setEnvPath("/maps/plains4.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 5:
                 setEnvPath("/maps/plains5.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 6:
                 setEnvPath("/maps/plains6.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 7:
                 setEnvPath("/maps/plains7.csv");
                 addItem(new Apple(), 15 * tileSize, 15 * tileSize);
                 addEnemy(new EN_RaiderPurple(getGp()), 14 * tileSize, 15 * tileSize);
+                addEvent(new Fever(0.0001));
                 break;
             case 8:
                 setEnvPath("/maps/plains8.csv");
